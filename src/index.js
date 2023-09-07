e4aecd57ce7afa5b59ad39485eb217b8cb498179
shwetapund <pundshweta2020@gmail.com>
2023-09-06 20:28:08 -0700

    inline css added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//props
+/*
+function Card(props)
+{
+  const {name, bestfriend} = props;
+  return(
+    <>
+    <h1>
+      hello i am {name}🤗.
+      <hr/>
+      Mein hu ek udta robo {name}🥰.
+      <hr/>
+      My Best Friend is {bestfriend}😊.
+    </h1>
+    </>
+  )
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <>
+ <Card name="doremon" bestfriend="Nobita"/>
+</>
 );
+*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//inline css -if we use inline css then wo use line par add karna hota hein .
+
+
+function School(props)
+{
+  const {name, age} = props;
+  return (
+    <>
+     <div>
+      <h1 style={{textAlign: 'center'}}>My Self</h1>
+      <hr/>
+      <h3>My name is {name}.</h3>
+      <h3>I am from {age}.</h3>
+      </div>
+    </>
+  )
+
+}
+
+root.render(
+  <>
+  <School name="shweta" age="21"/>
+  </>
+)
+
+//document level - if we use document css tab hum
+
+
+
+
